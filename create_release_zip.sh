67b8c83d591c30d0754ed46b8a6f020ad79e0418
j-fujimoto <hokuyo.jun@gmail.com>
2017-10-11 19:40:41 +0900

    Fixed create_release_zip.sh

--- create_release_zip.sh
+++ create_release_zip.sh
@@ -6,7 +6,9 @@ PACKAGE_NAME=${RELEASE_DIR}.zip
 copy_files () {
     targetDIR=${@:$#:1}
     targetFILE=${@:$#-1:1}
+    set -f
     echo targetDIR = ${targetDIR} targetFILE = ${targetFILE}
+    set +f
     if [ -e $1 ]; then
         for path in ${@:1:$#-2}
         do
@@ -19,15 +21,16 @@ copy_files () {
 
 rm -rf ${PACKAGE_NAME} ${RELEASE_DIR}
 mkdir ${RELEASE_DIR}
-copy_files include *.h ${RELEASE_DIR}
-copy_files samples *.c ${RELEASE_DIR}
-copy_files samples *.sh ${RELEASE_DIR}
+copy_files include '*.h' ${RELEASE_DIR}
+copy_files samples '*.c' ${RELEASE_DIR}
+copy_files samples '*.sh' ${RELEASE_DIR}
 copy_files samples Makefile ${RELEASE_DIR}
-copy_files src *.c ${RELEASE_DIR}
-copy_files vs20*/high* *.vcxproj ${RELEASE_DIR}
-copy_files vs20*/low* *.vcxproj ${RELEASE_DIR}
-copy_files vs20*/urg3d *.vcxproj ${RELEASE_DIR}
-copy_files vs20* *.sln ${RELEASE_DIR}
-copy_files windowsexe *.bat ${RELEASE_DIR}
+copy_files src '*.c' ${RELEASE_DIR}
+copy_files src Makefile ${RELEASE_DIR}
+copy_files vs20*/high* '*.vcxproj' ${RELEASE_DIR}
+copy_files vs20*/low* '*.vcxproj' ${RELEASE_DIR}
+copy_files vs20*/urg3d '*.vcxproj' ${RELEASE_DIR}
+copy_files vs20* '*.sln' ${RELEASE_DIR}
+copy_files windowsexe '*.bat' ${RELEASE_DIR}
 cp build_rule.mk *.txt Makefile *.pri *.in ${RELEASE_DIR}
 zip ${PACKAGE_NAME} ${RELEASE_DIR} -r
